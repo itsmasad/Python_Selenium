c9af99876e98fa61263e539dbaa9cfd1c6b784c5
itsmasad <muhammadasadofficial@gmail.com>
2024-02-01 20:11:05 +0500

    Working on conditionals in python

---
+++ Conditionals.py
@@ -0,0 +1,14 @@
+# If/ Else conditions are used to decide to do something based on something being true or false
+
+
+
+# Comparision Operators (==, !=, >, <, >=, <=) - Used to compare values
+
+
+
+# Logical Operators (and, or, not) - Used to combile conditional statements
+
+
+
+
+# Membership Operators (not, not in) - Membership operators are used to test if a sequence is presented in an object
