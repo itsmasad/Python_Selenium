5d2140fffe9e88b923c5fd2ee3192c8550b2ef5e
itsmasad <muhammadasadofficial@gmail.com>
2024-02-01 20:31:35 +0500

    Logical operators added

--- Conditionals.py
+++ Conditionals.py
@@ -1,5 +1,8 @@
 # If/ Else conditions are used to decide to do something based on something being true or false
 
+
+# Comparision Operators (==, !=, >, <, >=, <=) - Used to compare values
+
 x = 7
 y = 10
 
@@ -26,17 +29,21 @@ else:
 # Nasted if
     if x > 2:
         if x <= 10:
-          print(f'{x} is greater than 2 and less than or equal to {y}')  
-
-
-
-# Comparision Operators (==, !=, >, <, >=, <=) - Used to compare values
-
+          print(f'{x} is greater than 2 and less than or equal to {y}',': Nasted if condition')  
 
 
 # Logical Operators (and, or, not) - Used to combile conditional statements
 
+# And logical operator
+if x > 2 and x <= 10:
+    print(f'{x} is greater than 2 and less than or equal to {y}',': logical operator (And) condition')  
 
+# Or logical operator
+if x > 2 and x <= 10:
+    print(f'{x} is greater than 2 and less than or equal to {y}',': logical operator (Or) condition')  
 
+# Not logical operator
+if not(x == y):
+    print(f'{x} is not equal to {y}',': logical operator (Not) condition')  
 
 # Membership Operators (not, not in) - Membership operators are used to test if a sequence is presented in an object
