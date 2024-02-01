05dbf86c0a6866b84a6f9a57f8823d5b1983b0f5
itsmasad <muhammadasadofficial@gmail.com>
2024-02-01 20:24:47 +0500

    If alse conditions in conditionals

--- Conditionals.py
+++ Conditionals.py
@@ -1,5 +1,33 @@
 # If/ Else conditions are used to decide to do something based on something being true or false
 
+x = 7
+y = 10
+
+# Simple if
+
+if x > y:
+    print(f'{x} is greator then {y}', ': Simple if condition')
+
+# If / Else
+    
+if x > y:
+    print(f'{x} is greator then {y}', ': if else condition')
+else:
+    print(f'{x} is less then {y}', ': if else condition')
+
+# elif or else / if
+if x > y:
+    print(f'{x} is greator then {y}', ': elif or else / if condition')
+elif x == y:
+    print(f'{x} is equal to {y}', ': elif or else / if condition')
+else:
+    print(f'{x} is less then {y}', ': elif or else / if condition')
+
+# Nasted if
+    if x > 2:
+        if x <= 10:
+          print(f'{x} is greater than 2 and less than or equal to {y}')  
+
 
 
 # Comparision Operators (==, !=, >, <, >=, <=) - Used to compare values
