eaf9f123a6df9597fb6a9d2217612b4cb7ba1803
itsmasad <muhammadasadofficial@gmail.com>
2024-02-02 21:03:12 +0500

    Loops added

--- loops.py
+++ loops.py
@@ -30,3 +30,7 @@ for i in range(3, 11):
 
 # While loops execute a set of statements as long as a condition is true.
     
+count = 0
+while count <= 10:
+    print(f'Count : {count}')
+    count += 1
