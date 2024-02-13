28664d3dd7fefa5271f2c95aa0dd1fd8415828a6
itsmasad <muhammadasadofficial@gmail.com>
2024-02-13 19:37:31 +0500

    Classes

---
+++ Classes.py
@@ -0,0 +1,13 @@
+# A class is  like a blueprint for creating objects. An object has properties and methods (functions) associated with it. Almost everything in Python is an object
+
+# Create class
+class User:
+    # Constructor
+    def __init__(self, name, email, age):
+        self.name = name
+        self.email = email
+        self.age = age
+
+# Init
+brad = User('Brad Traversy', 'brad@gmail.com', 37)
+print('Asad')
